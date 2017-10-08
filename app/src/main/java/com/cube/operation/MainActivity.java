a19f33c88892e59161fc1337d8c9069e53c0bbd3
kenynw <692078286@qq.com>
2017-10-08 23:46:16 +0800

    init commit

---
+++ app/src/main/java/com/cube/operation/MainActivity.java
@@ -0,0 +1,32 @@
+package com.cube.operation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.cube.operation.account.LoginActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_main_login)
+    TextView mTvMainLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mTvMainLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/cube/operation/account/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.cube.operation.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cube.operation.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_activity_login);
+    }
+}
